092a72e780e76b57b58882b0a0c54fea0ac536a7
jplatt-cscc <jplatt@student.cscc.edu>
2025-05-09 21:40:07 -0400

    v2.0

--- ruby_repos_visual.py
+++ ruby_repos_visual.py
@@ -17,35 +17,40 @@ headers = {"Accept": "application/vnd.github.v3+json"}
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
 
-# Processes the results
+# Processes the results & checks if all requested data was recieved
 response_dict = r.json()
 print(f"Complete results: {not response_dict['incomplete_results']}")
 
-# Processes the repo information
+# Processes the repo information into different dicts
 repo_dicts = response_dict['items']
 repo_links, stars, hover_texts = [], [], []
 
 for repo_dict in repo_dicts:
-    """ Turns the repo names into active links """
+    """ Turns the repo names into active links & makes the hover over text """
+    # Gets the repo's name
     repo_name = repo_dict['name']
+    # Gets the repo's url
     repo_url = repo_dict['html_url']
+    # Formats the name and url into a clickable link with HTML
     repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
     repo_links.append(repo_link)
     
-    # Gets stars count for individual repos
+    # Gets the star count for individual repos
     stars.append(repo_dict['stargazers_count'])
 
-# Builds hover over texts
+    # Builds the hover over texts
     owner = repo_dict['owner']['login']
     description = repo_dict['description']
+    # Formats the hover over text with HTML
     hover_text = f"{owner}<br />{description}"
     hover_texts.append(hover_text)
 
-# Configures the plot
+""" Creating the bar graph section """
+# Sets the title & label text
 title = "Most Starred Ruby Projects on GitHub"
 labels = {'x': 'Repos', 'y': 'Stars'}
 
-# Creates the plot
+# Creates/initializes the graph
 fig = px.bar(x=repo_links, y=stars, title=title, labels=labels,
         hover_name=hover_texts)
 
@@ -53,11 +58,8 @@ fig = px.bar(x=repo_links, y=stars, title=title, labels=labels,
 fig.update_layout(title_font_size=36, xaxis_title_font_size=28,
         yaxis_title_font_size=28)
 
-# Configures the plot's bars
+# Configures the graph's bars
 fig.update_traces(marker_color='Red', marker_opacity=0.4)
 
 # Displays the plot
 fig.show()
-
-
-
